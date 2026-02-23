74eb57cd18382c119958c2e6759d648fe4bde1f1
nicolav93 <mr.questionmark93@gmail.com>
2026-02-23 11:18:08 +0100

    esercizio finito

--- discount.py
+++ discount.py
@@ -1,3 +1,21 @@
+def discount(prezzi, animale, numero):
+    risultato = 0.0
+    base_sconto = 0.0
+    numero_animali = 0
+    if len(set(animale)) == 2:
+        index = 0
+        for value in animale:
+            if value:
+                numero_animali = numero_animali + 1 
+            else:
+                base_sconto = base_sconto + prezzi[index]
+            index = index + 1
+        if (numero - numero_animali) >= 5:
+            risultato = base_sconto * 0.2
+    return risultato
+
+def main():
+
 prices = []
 isPet = []
 sconto = 0.0
@@ -14,3 +32,6 @@ with open ('lista_con_sconto.txt', 'r') as file:
     if nItems == len(isPet):
         sconto = discount(prices, isPet, nItems)
 print(sconto)
+
+if __name__ == "__main__":
+    main()
