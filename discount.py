e092c3eabac8805046ef43e0e5696f54d39fef43
nicolav93 <mr.questionmark93@gmail.com>
2026-02-23 11:20:38 +0100

    Refactor main function to improve readability

--- discount.py
+++ discount.py
@@ -16,22 +16,22 @@ def discount(prezzi, animale, numero):
 
 def main():
 
-prices = []
-isPet = []
-sconto = 0.0
-with open ('lista_con_sconto.txt', 'r') as file:
-    righe = file.readlines()
-    for riga in righe:
-        campi = riga.split()
-        prices.append(float(campi[0]))
-        if campi[1].upper() == 'Y':
-            isPet.append(True)
-        else:
-            isPet.append(False)
-    nItems = len(prices)
-    if nItems == len(isPet):
-        sconto = discount(prices, isPet, nItems)
-print(sconto)
+    prices = []
+    isPet = []
+    sconto = 0.0
+    with open ('lista_con_sconto.txt', 'r') as file:
+        righe = file.readlines()
+        for riga in righe:
+            campi = riga.split()
+            prices.append(float(campi[0]))
+            if campi[1].upper() == 'Y':
+                isPet.append(True)
+            else:
+                isPet.append(False)
+        nItems = len(prices)
+        if nItems == len(isPet):
+            sconto = discount(prices, isPet, nItems)
+    print(sconto)
 
 if __name__ == "__main__":
     main()
